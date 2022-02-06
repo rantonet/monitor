4956b498096ff6788bcbe05e983f451c4d1d335f
rantonet <roberto.antonetti@gmail.com>
2022-02-06 18:22:52 +0100

    commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# coding = utf-8
+# This Python file uses the following encoding: utf-8
 
 """
 This work is licensed under the Creative Commons Attribution 4.0 International
@@ -6,51 +6,51 @@ License. To view a copy of this license, visit
 http://creativecommons.org/licenses/by/4.0/ or send a letter to Creative
 Commons, PO Box 1866, Mountain View, CA 94042, USA.
 """
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    from multiprocessing  import Lock,Queue
-    from time             import time,sleep
-    import logging
-    import os
+from multiprocessing  import Lock,Queue
+from time             import time,sleep
+import logging
+import os
 
-    from gestore_pipeline import gestore_pipeline
+from gestore_pipeline import gestore_pipeline
 
 
 
-    ipc_entrata                 = Queue()
-    lock_ipc_entrata            = Lock()
-    ipc_uscita                  = Queue()
-    lock_ipc_uscita             = Lock()
-    file_configurazione         = "pipeline.conf"
-    file_log                    = "shotstation.log"
+ipc_entrata                 = Queue()
+lock_ipc_entrata            = Lock()
+ipc_uscita                  = Queue()
+lock_ipc_uscita             = Lock()
+file_configurazione         = "pipeline.conf"
+file_log                    = "shotstation.log"
 
-    segnale_entrata             = ""
-    segnale_uscita              = ""
-    segnale_uscita_spacchettato = []
+segnale_entrata             = ""
+segnale_uscita              = ""
+segnale_uscita_spacchettato = []
 
-    #if os.path.exists(file_log):
-    #      os.remove(file_log)
+#if os.path.exists(file_log):
+#      os.remove(file_log)
 
-    logging.basicConfig(filename=file_log,level=logging.INFO)
-    #logging.basicConfig(level=logging.DEBUG)
-    ######################## Codice Personale qui ##################################
-    p = gestore_pipeline(file_configurazione,
-                        ipc_uscita,
-                        lock_ipc_uscita,
-                        ipc_entrata,
-                        lock_ipc_entrata)
-    p.start()
+logging.basicConfig(filename=file_log,level=logging.INFO)
+#logging.basicConfig(level=logging.DEBUG)
+######################## Codice Personale qui ##################################
+p = gestore_pipeline(file_configurazione,
+                    ipc_uscita,
+                    lock_ipc_uscita,
+                    ipc_entrata,
+                    lock_ipc_entrata)
+p.start()
 
-    sleep(0.001)
-    with lock_ipc_uscita:
-        ipc_uscita.put_nowait("avvia:" + str(time()) + ":" + str(__name__) + ":gestore_pipeline")
-    sleep(0.001)
-    #
-    # while True:
-    #     comando      = input("Comando: ")
-    #     destinatario = input("Destinatario: ")
-    #
-    #     with lock_ipc_uscita:
-    #         ipc_uscita.put_nowait(comando + ":" + str(time()) + ":" + str(__name__) + ":" + destinatario)
+sleep(0.001)
+with lock_ipc_uscita:
+    ipc_uscita.put_nowait("avvia:" + str(time()) + ":" + str(__name__) + ":gestore_pipeline")
+sleep(0.001)
+#
+# while True:
+#     comando      = input("Comando: ")
+#     destinatario = input("Destinatario: ")
+#
+#     with lock_ipc_uscita:
+#         ipc_uscita.put_nowait(comando + ":" + str(time()) + ":" + str(__name__) + ":" + destinatario)
 
-    p.join()
+p.join()
